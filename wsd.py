5fdc3228017b3def6b7728c2e3d44494523649cc
Zachariah Dellimore <42615901+Party14534@users.noreply.github.com>
2025-03-12 14:03:42 -0400

    Finished training data parser

--- wsd.py
+++ wsd.py
@@ -2,50 +2,53 @@ import re
 
 
 class Sentence:
-    words = []
-    word_index = -1
+    sentence = ""
+    word = ""
     answer = ""
 
 
 training_data = []
 
-answer_pattern = r'<answer.*senseid="(.*)"/>'
+answer_pattern = r'<answer.*senseid="(.*)"\/>'
 answer_substitution = r'\1'
 
 sentence_pattern = r'<s>(.*)<head>(.*)</head>(.*)</s>'
 sentence_substitution = r'\1 \2 \3'
+
+word_pattern = r'(\s|\S)*<head>(.*)<\/head>(\s|\S)*'
+word_substitution = r'\2'
 
 
 def load_training_data(filename):
     file = open(filename, 'r')
     data = file.read()
 
-    # Get all answers
-    answer_units = re.findall(r'<answer.*/>', data)
-    answers = []
-    for answer_unit in answer_units:
-        answer = re.sub(answer_pattern, answer_substitution, answer_unit)
-        answers.append(answer)
-
-    # Get the sentences
+    # Get the sentences and answers
     sentences = []
-    context_units = re.findall(r'<context>(\s*<s>.*<\/s>\s*)<\/context>', data)
-    for context_unit in context_units:
-        sentence_units = re.findall(r'<s>.*</s>', context_unit)
+    data_units = re.findall(r'<answer.*/>\s<context>\s*<s>.*<\/s>\s*<\/context>', data)
+    for data_unit in data_units:
+        sentence_units = re.findall(r'<s>.*</s>', data_unit)
         sentence_unit = ""
         for s in sentence_units:
             s = re.sub(r'<.*?>', '', s)
             sentence_unit += s
 
+        s = Sentence()
+
         sentence = re.sub(sentence_pattern, sentence_substitution,
                           sentence_unit)
-        sentences.append(sentence)
-        print(sentence)
+        s.sentence = sentence
 
+        answer_unit = re.findall(r'<answer.*/>', data_unit)
+        answer = re.sub(answer_pattern, answer_substitution, answer_unit[0])
+        s.answer = answer
 
-    print(len(sentences))
-    print(len(context_units))
-    print(len(answers))
+        word = re.sub(word_pattern, word_substitution, data_unit)
+        s.word = word
+
+        print(sentence, "\n", answer, "\n", word, "\n----------")
+
+        sentences.append(s)
 
 
 def main():
